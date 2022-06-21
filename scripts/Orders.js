a7de4cf7ab5cdd9c9a2b8edb6b84b3b41a4ea82f
Mason Thieman <masonthieman@gmail.com>
2022-06-21 10:52:46 -0500

    Add order costs

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,5 +1,35 @@
 import { getOrders } from "./database.js"
+import { getPaints, getInteriors, getTechnologies, getWheels } from "./database.js"
 
+export const findOrderPrices = (order) => {
+    const interiors = getInteriors()
+    const paints = getPaints()
+    const techs = getTechnologies()
+    const wheels = getWheels()
+
+    const foundInterior = interiors.find(interior => {
+        return interior.id === order.interiorId
+    })
+
+    const foundPaint = paints.find(paint => {
+        return paint.id === order.paintId
+    })
+
+    const foundTechs = techs.find(tech => {
+        return tech.id === order.techId
+    })
+
+    const foundWheels = wheels.find(wheel => {
+        return wheel.id === order.wheelId
+    })
+
+    let orderPrice = foundInterior.price + foundPaint.price + foundTechs.price + foundWheels.price
+
+    return orderPrice.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+}
 
 export const Orders = () => {
     const orders = getOrders()
@@ -8,7 +38,8 @@ export const Orders = () => {
 
     for (const order of orders) {
         html += `<li id="${order.id}">`
-        html += `Order #${order.id} was placed on ${order.timestamp} </li>`
+        const totalPrice = findOrderPrices(order)
+        html += `Order #${order.id} costs ${totalPrice} </li>`
     }
 
     html += "</ul>"
